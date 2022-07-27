0213aa9954b238a199510cb8e76db39423dc8520
frsargua <frsargua@gmail.com>
2022-07-27 12:09:52 +0100

    added click event to remove card from the viewport

--- Assets/index.js
+++ Assets/index.js
@@ -1,8 +1,14 @@
-$(window).scroll(function () {
-  let scroll = $(window).scrollTop();
-  if (scroll >= $("#porfolio").offset().top) {
-    $("body").css("background-color", "black");
-  } else {
-    $("body").fadeIn().css("background-color", "white");
-  }
+// $(window).scroll(function () {
+//   let scroll = $(window).scrollTop();
+//   if (scroll >= $("#porfolio").offset().top) {
+//     $("body").css("background-color", "black");
+//   } else {
+//     $("body").fadeIn().css("background-color", "white");
+//   }
+// });
+
+let closingIconEl = $("#closing-icon-description-card");
+
+closingIconEl.click(function () {
+  $(this).parent().parent().removeClass("d-flex").addClass("d-none");
 });
