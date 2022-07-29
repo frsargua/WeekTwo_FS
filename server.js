066bf9af8a46999a801d3468cb661fc3b3338eb5
frsargua <frsargua@gmail.com>
2022-07-29 16:24:02 +0100

    updated description of projects

--- server.js
+++ server.js
@@ -17,18 +17,29 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let largeProjects = [
   {
-    repoUrl: "https://frsargua.github.io/week10-HW/",
+    repoUrl: "https://github.com/frsargua/STAYHAVEN-APP",
+    deployed: "https://radiant-bayou-46133.herokuapp.com",
     imageUrl: "./Images/stayHaven.png",
     title: "StayHaven",
-    description: "A booking, listing and search site for short stays.",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Node.js",
+      "BootStrap",
+      "Express.js",
+      "RESTFUL-APIs",
+    ],
+    description:
+      "A booking, listing and search site for short stays which requires the user to sign in/up to access the functionalities.",
   },
   {
-    repoUrl: "https://ramielsayed.github.io/film-go/",
+    repoUrl: "https://github.com/RamiElsayed/film-go",
+    deployed: "https://ramielsayed.github.io/film-go/",
     imageUrl: "./Images/film-Go.png",
     title: "Film-Go App",
-    description: {
-      description: "Films",
-    },
+    description:
+      "A movie search website to bookmark, search by genre and name, any movies supported by IMDB-API library",
   },
 ];
 let smallProjects = [
@@ -36,19 +47,27 @@ let smallProjects = [
     repoUrl: "https://frsargua.github.io/week10-HW/",
     imageUrl: "./Images/teamprofile.png",
     title: "Team Profile Generator",
+    description:
+      "A terminal app to create a html page containing the members of a team and their positions ",
   },
   {
-    repoUrl: "https://frsargua.github.io/WeekSix_FS/",
+    repoUrl: "https://github.com/frsargua/WeekSix_FS",
+    deployed: "https://frsargua.github.io/WeekSix_FS/",
     imageUrl: "./Images/wheather-app.png",
-    title: "Wheather App",
+    title: "Weather App",
+    description:
+      "The app allows the user to search for the weather forecast for a given location and saves it to the local storage.",
   },
   {
-    repoUrl: "https://frsargua.github.io/WeekSix_FS/",
+    repoUrl: "https://github.com/frsargua/WeekFourHW_FS",
+    deployed: "https://frsargua.github.io/WeekFourHW_FS/",
     imageUrl: "./Images/quizz-app.png",
-    title: "Quizz App",
+    title: "Quiz App",
+    description: "A short quiz app",
   },
   {
-    repoUrl: "https://frsargua.github.io/WeekFive_FS/",
+    repoUrl: "https://github.com/frsargua/WeekFive_FS",
+    deployed: "https://frsargua.github.io/WeekFive_FS/",
     imageUrl: "./Images/Scheduler-app.png",
     title: "Scheduler App",
   },
